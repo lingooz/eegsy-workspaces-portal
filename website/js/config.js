67d5d99739e273e0a93a75cf5fbd5219ffca214f
Sonic Yang <sonic.yang@schimatech.com.tw>
2022-08-16 18:50:15 +0800

    fix typo in web site settings

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-1_RflvdnR5L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'eu-west-1_RflvdnR5L', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2th4dtbi0tu703960audv542sh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
